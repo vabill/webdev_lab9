bb5da9c34787b01df0b5bd3f99f359ae5e7dd3df
vabill <vbill@hawk.iit.edu>
2024-11-03 12:48:23 -0600

    Create script.js
    added necessary code

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function init(){
+	var button = document.getElementById('entrybutton');
+	function showMeText(){
+		var textbox = document.getElementById('entryinput');
+		document.getElementById('textoutput').innerHTML = textbox.value
+		alert("Alastyr Bill: " + textbox.value);
+	}
+	button.addEventListener('click', showMeText);
+}
+window.addEventListener('load', init)
